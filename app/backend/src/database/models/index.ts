b3c244d74dab0f4ab19fbee32fb5257f03683e09
Arthur Teixeira <tuzinf5@gmail.com>
2022-08-17 18:25:07 -0300

    docs: Explicação da instância do Sequelize

--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -1,3 +1,5 @@
+// Antes, esse arquivo era gerado automaticamente e já fazia o reconhecimento dos models gerados pelo sequelize-cli.
+// No nosso caso, esse arquivo servirá única e exclusivamente para gerar e exportar uma nova instância do Sequelize, baseada na configuração anterior (já assegurada pela tipagem):
 import { Sequelize } from 'sequelize';
 import * as config from '../config/database';
 
