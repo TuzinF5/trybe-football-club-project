606132966e4c742f0d1e6bc7c5bafd676bf8efdf
Arthur Teixeira <tuzinf5@gmail.com>
2022-08-18 12:03:34 -0300

    feat(Models): Criando o modelo Team para a tabelas teams

---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,30 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Team.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: INTEGER,
+    },
+    teamName: {
+      allowNull: false,
+      type: STRING,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'teams',
+    timestamps: false,
+    underscored: true,
+  },
+);
+
+export default Team;
