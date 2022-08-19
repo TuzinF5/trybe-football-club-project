b3cd1676b16e68fe8e10ba93a5de79d45f877f1e
Arthur Teixeira <tuzinf5@gmail.com>
2022-08-19 14:44:05 -0300

    feat(Controllers): Criando função loginValidate

--- app/backend/src/controllers/LoginController.ts
+++ app/backend/src/controllers/LoginController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
+import JwtService from '../services/JwtService';
 import { IStatusMessage } from '../interfaces/IStatusMessage';
 import { ILoginService } from '../interfaces/loginInterfaces';
+import { IUserPayloadJwt } from '../interfaces/IUser';
 
 export default class LoginController {
   constructor(public loginService: ILoginService) {}
@@ -17,6 +19,24 @@ export default class LoginController {
       }
 
       return res.status(200).json({ token });
+    } catch (err) {
+      const error = err as Error;
+      console.log(error.message);
+      return res.status(500).json({ message: error.message });
+    }
+  }
+
+  static async loginValidate(req: Request, res: Response) {
+    try {
+      const { authorization } = req.headers;
+
+      if (!authorization) {
+        return res.status(401).json({ message: 'Token does not exist' });
+      }
+
+      const { role } = JwtService.decode(authorization) as IUserPayloadJwt;
+
+      return res.status(200).json({ role });
     } catch (err) {
       const error = err as Error;
       console.log(error.message);
