2cde9c17b3a055da2e9423539d14f06874f327ba
Arthur Teixeira <tuzinf5@gmail.com>
2022-08-19 16:36:50 -0300

    Criando mock para os testes

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -8,22 +8,11 @@ import { app } from '../app';
 import JwtService from '../services/JwtService';
 import BcryptService from '../services/BcrptService';
 import ValidateRequestBody from '../middlewares/ValidateRequestBody';
+import { tokenMock, validData } from './mocks/login';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
-
-const validData = {
-  id: 1,
-  username: 'Admin',
-  role: 'admin',
-  email: 'admin@admin.com',
-  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
-  // senha: secret_admin
-};
-
-const tokenMock =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJpYXQiOjE2NjA4NTM1MjB9.LmylBI6m8p9PUtdUOLETY_SlQFJwKDEdalfOHAPJj2E';
 
 describe('Rota de Login', () => {
   afterEach(() => {
---
+++ app/backend/src/tests/mocks/login.ts
@@ -0,0 +1,13 @@
+const validData = {
+  id: 1,
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+  // senha: secret_admin
+};
+
+const tokenMock =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJpYXQiOjE2NjA4NTM1MjB9.LmylBI6m8p9PUtdUOLETY_SlQFJwKDEdalfOHAPJj2E';
+
+export { validData, tokenMock };
