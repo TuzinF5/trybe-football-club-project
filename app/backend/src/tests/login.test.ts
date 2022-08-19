c200d34ab365159ee896c2f19a4c48542f64a650
Arthur Teixeira <tuzinf5@gmail.com>
2022-08-19 09:46:19 -0300

    test(Tests): Adicionando testes para a rota de POST /login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -24,16 +24,16 @@ const validData = {
 const tokenMock =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJpYXQiOjE2NjA4NTc3Njl9.O5BSakwxUBcu2Bn6n1wC2ukxJkq6y7EoHR1CpOAzOs0';
 
-describe('Login', () => {
+describe('Rota de Login', () => {
+  afterEach(() => {
+    Sinon.restore();
+  });
+
   describe('Quando acessar a rota "/login" com dados válidos no front-end', () => {
     beforeEach(() => {
       Sinon.stub(User, 'findOne').resolves(validData as User);
       Sinon.stub(BcryptService, 'compare').resolves(true);
       Sinon.stub(JwtService, 'sign').returns(tokenMock);
-    });
-
-    afterEach(() => {
-      Sinon.restore();
     });
 
     it('A rota deve retornar um status http 200', async () => {
@@ -53,6 +53,124 @@ describe('Login', () => {
 
       expect(response.body).to.be.haveOwnProperty('token');
       expect(response.body.token).to.be.a('string');
+    });
+  });
+
+  describe('Quando acessar a rota "/login" com dados inválidos no front-end', () => {
+    describe('Sem o campo email', () => {
+      it('A rota deve retornar um status http 400', async () => {
+        const response = await chai.request(app).post('/login').send({
+          password: 'secret_admin',
+        });
+
+        expect(response.status).to.be.equal(400);
+      });
+
+      it('A rota deve retornar no corpo da resposta "{ message: "All fields must be filled" }"', async () => {
+        const response = await chai.request(app).post('/login').send({
+          password: 'secret_admin',
+        });
+
+        expect(response.body).to.be.haveOwnProperty('message');
+        expect(response.body.message).to.be.a('string');
+        expect(response.body.message).to.be.equal('All fields must be filled');
+      });
+    });
+
+    describe('Sem o campo password', () => {
+      it('A rota deve retornar um status http 400', async () => {
+        const response = await chai.request(app).post('/login').send({
+          email: 'admin@admin.com',
+        });
+
+        expect(response.status).to.be.equal(400);
+      });
+
+      it('A rota deve retornar no corpo da resposta "{ message: "All fields must be filled" }"', async () => {
+        const response = await chai.request(app).post('/login').send({
+          email: 'admin@admin.com',
+        });
+
+        expect(response.body).to.be.haveOwnProperty('message');
+        expect(response.body.message).to.be.a('string');
+        expect(response.body.message).to.be.equal('All fields must be filled');
+      });
+    });
+
+    describe('Com um email no formato inválido', () => {
+      it('A rota deve retornar um status http 401', async () => {
+        const response = await chai.request(app).post('/login').send({
+          email: 'admin.com',
+          password: 'secret_admin',
+        });
+
+        expect(response.status).to.be.equal(401);
+      });
+
+      it('A rota deve retornar no corpo da resposta "{ message: "Email is not in the correct format" }"', async () => {
+        const response = await chai.request(app).post('/login').send({
+          email: 'admin.com',
+          password: 'secret_admin',
+        });
+
+        expect(response.body).to.be.haveOwnProperty('message');
+        expect(response.body.message).to.be.a('string');
+        expect(response.body.message).to.be.equal(
+          'Email is not in the correct format'
+        );
+      });
+    });
+
+    describe('Com um password no formato inválido', () => {
+      it('A rota deve retornar um status http 401', async () => {
+        const response = await chai.request(app).post('/login').send({
+          email: 'admin@admin.com',
+          password: 'secre',
+        });
+
+        expect(response.status).to.be.equal(401);
+      });
+
+      it('A rota deve retornar no corpo da resposta "{ message: "Password must be at least 6 characters long" }"', async () => {
+        const response = await chai.request(app).post('/login').send({
+          email: 'admin@admin.com',
+          password: 'secre',
+        });
+
+        expect(response.body).to.be.haveOwnProperty('message');
+        expect(response.body.message).to.be.a('string');
+        expect(response.body.message).to.be.equal(
+          'Password must be at least 6 characters long'
+        );
+      });
+    });
+
+    describe('Com um email inválido', () => {
+      beforeEach(() => {
+        Sinon.stub(User, 'findOne').resolves(null);
+      });
+
+      it('A rota deve retornar um status http 401', async () => {
+        const response = await chai.request(app).post('/login').send({
+          email: 'admin@xablau.com',
+          password: 'secret_admin',
+        });
+
+        expect(response.status).to.be.equal(401);
+      });
+
+      it('A rota deve retornar no corpo da resposta "{ message: "Incorrect email or password" }"', async () => {
+        const response = await chai.request(app).post('/login').send({
+          email: 'admin@admin.com',
+          password: 'secret_admin',
+        });
+
+        expect(response.body).to.be.haveOwnProperty('message');
+        expect(response.body.message).to.be.a('string');
+        expect(response.body.message).to.be.equal(
+          'Incorrect email or password'
+        );
+      });
     });
   });
 });
