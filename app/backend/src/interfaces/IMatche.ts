9dde196706600d035667e041809e7d5422fdf06b
Arthur Teixeira <tuzinf5@gmail.com>
2022-08-22 16:24:28 -0300

    feat(Interfaces): Criando intarface IMatche

--- app/backend/src/interfaces/IMatche.ts
+++ app/backend/src/interfaces/IMatche.ts
@@ -1,6 +1,14 @@
 import Matche from '../database/models/Matche';
 
+export interface IMatche {
+  homeTeam: number;
+  awayTeam: number;
+  homeTeamGoals: number;
+  awayTeamGoals: number;
+}
+
 export interface IMatcheService {
   findAll(): Promise<Matche[]>;
   searchByTerm(inProgress: boolean): Promise<Matche[]>;
+  create(data: IMatche): Promise<Matche>;
 }
