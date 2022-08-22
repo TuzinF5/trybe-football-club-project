d9b72fbd21004dde34329f0622a419b89674d0a7
Arthur Teixeira <tuzinf5@gmail.com>
2022-08-22 10:32:43 -0300

    feat(Services): Criando a classe MatcheService

---
+++ app/backend/src/services/MatcheService.ts
@@ -0,0 +1,17 @@
+import Team from '../database/models/Team';
+import Matche from '../database/models/Matche';
+
+export default class MatcheService {
+  private _matches: Promise<Matche[]>;
+
+  public async findAll() {
+    this._matches = Matche.findAll({
+      include: [
+        { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+    });
+
+    return this._matches;
+  }
+}
