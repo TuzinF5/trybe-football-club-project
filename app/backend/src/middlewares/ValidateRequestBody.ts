1a2727cc083a275a5b508d0412f3bf2b14354ed0
Arthur Teixeira <tuzinf5@gmail.com>
2022-08-24 13:43:00 -0300

    refactor(Middlewares): Refatorando para lidar melhor com os erros

--- app/backend/src/middlewares/ValidateRequestBody.ts
+++ app/backend/src/middlewares/ValidateRequestBody.ts
@@ -1,18 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 import { Secret } from 'jsonwebtoken';
+import HandleTheError from '../utils/HandleTheError';
 import JwtService from '../services/JwtService';
-import { IStatusMessage } from '../interfaces/IStatusMessage';
 
 export default class ValidateRequestBody {
   private static _jwtSecret: Secret = process.env.JWT_SECRET as Secret;
   private static _emailValidate = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/im;
 
-  static validateToken(req: Request, res: Response, next: NextFunction) {
+  public static validateToken(req: Request, res: Response, next: NextFunction) {
     try {
       const { authorization } = req.headers;
 
       if (!authorization) {
-        return res.status(401).json({ message: 'Token does not exist' });
+        throw new HandleTheError(400, 'Token does not exist!');
       }
 
       JwtService.verify(authorization, ValidateRequestBody._jwtSecret);
@@ -25,49 +25,28 @@ export default class ValidateRequestBody {
     }
   }
 
-  static validateEmail(email: string): IStatusMessage | boolean {
+  public static validateEmail(email: string): void {
     if (!email) {
-      return { status: 400, message: 'All fields must be filled' };
+      throw new HandleTheError(400, 'All fields must be filled');
     }
     if (!ValidateRequestBody._emailValidate.test(email)) {
-      return { status: 400, message: 'The email is not in the correct format!' };
+      throw new HandleTheError(400, 'The email is not in the correct format!');
     }
-    return true;
   }
 
-  static validatePassword(password: string): IStatusMessage | boolean {
+  public static validatePassword(password: string): void {
     if (!password) {
-      return { status: 400, message: 'All fields must be filled' };
+      throw new HandleTheError(400, 'All fields must be filled');
     }
     if (password.length < 6) {
-      return {
-        status: 400,
-        message: 'Password must be longer than 6 characters!',
-      };
+      throw new HandleTheError(400, 'Password must be longer than 6 characters!');
     }
-    return true;
   }
 
-  static loginRequestBody(req: Request, res: Response, next: NextFunction) {
-    try {
-      const validatedEmailResult = ValidateRequestBody.validateEmail(req.body.email);
-      const validatedPasswordResult = ValidateRequestBody.validatePassword(req.body.password);
+  public static validateLogin(req: Request, _res: Response, next: NextFunction): void {
+    ValidateRequestBody.validateEmail(req.body.email);
+    ValidateRequestBody.validatePassword(req.body.password);
 
-      if (typeof validatedEmailResult === 'object') {
-        return res.status(validatedEmailResult.status)
-          .json({ message: validatedEmailResult.message });
-      }
-
-      if (typeof validatedPasswordResult === 'object') {
-        return res.status(validatedPasswordResult.status)
-          .json({ message: validatedPasswordResult.message });
-      }
-
-      return next();
-    } catch (err) {
-      const error = err as Error;
-      console.log(error.message);
-      return res.status(500).json({ message: error.message });
-    }
+    return next();
   }
 }
