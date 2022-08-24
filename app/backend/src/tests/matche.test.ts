ef99acbde6717958c7c9d143a9fee19470a9ba90
Arthur Teixeira <tuzinf5@gmail.com>
2022-08-24 17:15:18 -0300

    test(Tests): Adicionando testes a rota /matches

--- app/backend/src/tests/matche.test.ts
+++ app/backend/src/tests/matche.test.ts
@@ -4,41 +4,11 @@ import * as Sinon from 'sinon';
 import chaiHttp = require('chai-http');
 import { app } from '../app';
 import Matche from '../database/models/Matche';
+import { matchesMock, matchesMockInProgressrue } from './mocks/matche';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
-
-const matchesMock = [
-  {
-    id: 1,
-    homeTeam: 16,
-    homeTeamGoals: 3,
-    awayTeam: 8,
-    awayTeamGoals: 1,
-    inProgress: false,
-    teamHome: {
-      teamName: 'São Paulo',
-    },
-    teamAway: {
-      teamName: 'Grêmio',
-    },
-  },
-  {
-    id: 2,
-    homeTeam: 9,
-    homeTeamGoals: 1,
-    awayTeam: 14,
-    awayTeamGoals: 1,
-    inProgress: false,
-    teamHome: {
-      teamName: 'Internacional',
-    },
-    teamAway: {
-      teamName: 'Santos',
-    },
-  },
-];
 
 describe('Rota de Matche', () => {
   afterEach(() => {
@@ -56,10 +26,29 @@ describe('Rota de Matche', () => {
       expect(response.status).to.be.equal(200);
     });
 
-    it('A rota deve retornar um array com todas as partidas', async () => {
+    it('A rota deve retornar um array com todas as partidas finalizadas', async () => {
       const response = await chai.request(app).get('/matches');
 
       expect(response.body).to.be.eql(matchesMock);
+      expect(response.body).to.be.a('array');
+    });
+  });
+
+  describe('Quando acessar a rota GET "matches?inProgress=true"', () => {
+    beforeEach(() => {
+      Sinon.stub(Matche, 'findAll').resolves(matchesMockInProgressrue as Matche[]);
+    });
+
+    it('A rota deve retornar um status http 200', async () => {
+      const response = await chai.request(app).get('/matches?inProgress=truee');
+
+      expect(response.status).to.be.equal(200);
+    });
+
+    it('A rota deve retornar um array com todas as partidas em andamento', async () => {
+      const response = await chai.request(app).get('/matches');
+
+      expect(response.body).to.be.eql(matchesMockInProgressrue);
       expect(response.body).to.be.a('array');
     });
   });
